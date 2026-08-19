5d91f79c08c5f42fb8f1d386735db5da9e8c638a
HassanMahdee <hasanmahdee013@gmail.com>
2026-08-19 18:55:53 +0600

    fix: added missing packages

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI as string);
+const db = client.db(process.env.MONGODB_DB_NAME as string);
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, { client }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "student",
+      },
+    },
+  },
+});
